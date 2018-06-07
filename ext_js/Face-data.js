d69682ece1690d83a4bfafcbec45671ec5b8b992
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-06-07 19:19:11 +0000

    graph is not showing yet

--- ext_js/Face-data.js
+++ ext_js/Face-data.js
@@ -1,18 +1,30 @@
 queue()
- .defer(d3.csv,"data/data.csv")
+ .defer(d3.csv,"Data/data.csv")
  .await(makeGraphs);
   
   function makeGraphs (error, Photosdata){
       var ndx=crossfilter(Photosdata);
       
       show_data_bar(ndx);
+      show_select_bar(ndx);
        
        dc.renderAll;
   }
   
+  function show_select_bar(ndx){
+      var dim = ndx.dimension(dc.pluck('proffesional'));
+     var group = dim.group();
+     
+     dc.selectMenu("#selector")
+     .dimension(dim)
+     .group(group);
+  }
+  
+  
+  
   function show_data_bar(ndx){
-      var name_dim= ndx.dimension(dc.pluck('sex'));
-      var name_group=name_dim.group().reduceSum();
+      var name_dim= ndx.dimension(dc.pluck("sex"));
+      var name_group=name_dim.group().reduceSum(dc.pluck("Photos"));
       
       dc.barChart("#chart_one")
        .width(400)
@@ -21,8 +33,8 @@ queue()
        .dimension(name_dim)
        .group(name_group)
        .transitionDuration(500)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
+     .x(d3.scale.ordinal())
+     .xUnits(dc.units.ordinal)
     
     .xAxisLabel("Gender")
         .yAxis().ticks(20);
